bd71d10414dbf30978e69f6dce9ee24f9e9127a9
Juanesduque1 <juanesduquemorales@gmail.com>
2023-01-24 17:43:56 -0500

    Task 12

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def fizzbuzz():
+    for i in range(1,101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=' ')
+            continue
+        elif i % 5 == 0:
+            print("Buzz", end=' ')
+            continue
+        elif i % 3 == 0:
+            print("Fizz", end=' ')
+            continue
+        else:
+            print(f"{i}", end='')
+        if i != 100:
+            print(' ', end='')
+            continue
+        return
