bab836c6cfe125407fe3c253d7b61391ebd84596
Juanesduque1 <juanesduquemorales@gmail.com>
2023-01-26 10:06:56 -0500

    Task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,6 +2,7 @@
 
 from add_0 import add
 
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
