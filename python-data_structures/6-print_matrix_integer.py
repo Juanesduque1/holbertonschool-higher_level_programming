f9e4c756236634efb5726424b7fc6a74481b2492
Juanesduque1 <juanesduquemorales@gmail.com>
2023-01-30 12:01:03 -0500

    Task 6 correction

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
-            print("{:d}".format(j), end=" ")
+            print("{:d} ".format(j))
         print()
