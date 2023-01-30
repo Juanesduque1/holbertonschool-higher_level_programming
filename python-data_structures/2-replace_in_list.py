35dd32480cec9e4e16ccb9f1944818e6e875bbf6
Juanesduque1 <juanesduquemorales@gmail.com>
2023-01-30 10:22:44 -0500

    Task 2

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def replace_in_list(my_list, idx, element):
+    if idx >= len(my_list) or idx < 0:
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
+
