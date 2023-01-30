1b8738be5c47b663ebfb6c9b87d37dcbfe99d615
Juanesduque1 <juanesduquemorales@gmail.com>
2023-01-30 11:10:23 -0500

    Task 5 correction

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,6 +2,4 @@
 
 
 def no_c(my_string):
-    new_string = my_string.replace('c', '')
-    new_string = new_string.replace('C', '')
-    return new_string
+    return my_string.translate({ord(i): None for i in 'cC'})
