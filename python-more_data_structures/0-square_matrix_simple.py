1bd6189a624bd144c241e03d30adb99d97262599
Juanesduque1 <juanesduquemorales@gmail.com>
2023-01-31 13:02:52 -0500

    PEP8 correction

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    copy = (list(map(lambda x :list(map(lambda y: (y**2), x)), matrix )))
+    copy = list(map(lambda x : list(map(lambda y : (y**2), x)), matrix))
     return copy
