6b2e88a0200b5220ae2bbfaf0336577ec0107c29
Juanesduque1 <juanesduquemorales@gmail.com>
2023-01-31 15:23:38 -0500

    Task 8

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    return a_dictionary.pop(key, None)
+    if key in a_dictionary:
+            a_dictionary.pop(key)
+    return  a_dictionary
