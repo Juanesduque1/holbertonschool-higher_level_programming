420dd3bac981e51e9dd110195b24c19b48bf66b0
Juanesduque1 <juanesduquemorales@gmail.com>
2023-01-31 17:07:49 -0500

    PEP8 correction

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,10 +2,10 @@
 def roman_to_int(roman_string):
     if type(roman_string) != str:
         return 0
-    rnumerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     sum = 0
-    for i in range(len(roman_string) -1, -1, -1):
-            result = rnumerals[roman_string[i]]
+    for i in range(len(roman_string) - 1, -1, -1):
+            result = roman[roman_string[i]]
             if 3 * result < sum:
                 sum = sum - result
             else:
