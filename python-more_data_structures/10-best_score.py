9e6c2c30108baf1f5ec69fd1c47713e9b9e478b7
Juanesduque1 <juanesduquemorales@gmail.com>
2023-01-31 15:55:30 -0500

    Task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,5 +3,5 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     else:
-        maxresult = max(a_dictionary)
+        maxresult = max(a_dictionary, key=a_dictionary.get)
     return maxresult
