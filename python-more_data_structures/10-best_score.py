bc1692fcf504652896301ea486f0b4432aac7bc9
Juanesduque1 <juanesduquemorales@gmail.com>
2023-01-31 16:02:35 -0500

    Task 10 correction

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
-    else:
+    if a_dictionary:
         maxresult = max(a_dictionary, key=a_dictionary.get)
+    else:
+        return None
     return maxresult
