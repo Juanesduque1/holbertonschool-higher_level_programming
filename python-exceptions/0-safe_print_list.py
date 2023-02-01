a62f5911a37e5ad2c763a14d43b96a356f5e2476
Juanesduque1 <juanesduquemorales@gmail.com>
2023-02-01 18:35:51 -0500

    Task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,8 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
             count += 1
-        print()
-        return count
-    except Exception:
-        print()
-        return count
+    except IndexError:
+        pass
+    print()
+    return count
