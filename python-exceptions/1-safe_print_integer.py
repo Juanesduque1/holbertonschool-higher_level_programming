adc6eb29424bf0e88ebba1288dca5e95466a203e
Juanesduque1 <juanesduquemorales@gmail.com>
2023-02-01 18:59:54 -0500

    Task 1 correction

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    try: 
+    try:
         print("{:d}".format(value))
         return True
-    except ValueError:
+    except (ValueError, TypeError):
         return False
