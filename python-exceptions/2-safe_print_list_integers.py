305a71e40c1b0bdc2a0717b196a3844742e5c90d
Juanesduque1 <juanesduquemorales@gmail.com>
2023-02-01 19:14:51 -0500

    Task 2 correction

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             count += 1
         except (TypeError, ValueError):
             continue
-        except IndexError:
-            break
     print()
     return count
