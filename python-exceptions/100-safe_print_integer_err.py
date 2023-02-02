2bc3cbc9b090cb08219f8e35f05eff881ed1565d
Juanesduque1 <juanesduquemorales@gmail.com>
2023-02-02 18:31:35 -0500

    Advanced task 100

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -7,18 +7,3 @@ def safe_print_integer_err(value):
     except ValueError as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return False
-
-value = 89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = -89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = "School"
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
