43a5460924bdb0ad54390b38c455b64ad7e30ba9
Juanesduque1 <juanesduquemorales@gmail.com>
2023-02-07 10:56:45 -0500

    [Fix]: Pycodestyle corrected

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -24,7 +24,7 @@ class Square:
         Allows to see the size of the square
         """
         if type(value) is not int:
-            raise TypeError("value must be an integer")
+            raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
