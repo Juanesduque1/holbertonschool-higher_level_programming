d10f9b969c948b1d8d40eee5ea1245450ff3d170
Juanesduque1 <juanesduquemorales@gmail.com>
2023-02-07 10:55:35 -0500

    [Feature]: Class and its properties added

---
+++ python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Defining a class that contains a Square"""
+
+
+class Square:
+    """Class containing information about the square"""
+    def __init__(self, size=0):
+        """
+        Initialization of object
+        with size as private attribute
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Returns size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Allows to see the size of the square
+        """
+        if type(value) is not int:
+            raise TypeError("value must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Returns area of the square
+        """
+        return self.__size ** 2
