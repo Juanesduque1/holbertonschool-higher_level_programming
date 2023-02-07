19399b3158682d756772ee97fc4b9d42d7a57b1a
Juanesduque1 <juanesduquemorales@gmail.com>
2023-02-07 00:21:50 -0500

    [Fix]: PEP8 stylecode corrected

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -12,5 +12,5 @@ class Square:
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
-           raise ValueError("size must be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = size
