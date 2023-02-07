79a15b8dcbcf22831b2a1ac430ebc16ebc226f0d
Juanesduque1 <juanesduquemorales@gmail.com>
2023-02-07 00:06:12 -0500

    [Feature]: Class added

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defining a class that contains a Square"""
+
+
+class Square:
+    """Class containing information about the square"""
+    def __init__(self, size):
+        """
+        Initialization of object
+        with size private attribute
+        """
+        self.__size = size
