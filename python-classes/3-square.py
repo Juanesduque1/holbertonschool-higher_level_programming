c592403cddf65376fde3c5db48fdca9b44753007
Juanesduque1 <juanesduquemorales@gmail.com>
2023-02-07 00:43:43 -0500

    [Fix]: Pycodestyle corrected

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -14,7 +14,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    
+
     def area(self):
         """
         Returns area of the square
