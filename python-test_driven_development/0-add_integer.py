99647c89127720a70735163ac90bbc128afe27a4
Juanesduque1 <juanesduquemorales@gmail.com>
2023-02-09 16:33:17 -0500

    [Feature]: Task finished

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """Function that adds two integers"""
+    """
+    Args:
+        a = int
+        b = int
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be integer")
+
+    a, b = int(a),  int(b)
+    return a + b
+
+add_integer()
---
+++ python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+Readme for test driven development in Python
