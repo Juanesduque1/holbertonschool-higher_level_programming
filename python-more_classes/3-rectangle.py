75b0e9caed70ceabda7a0f4a96abdeab2e7ddbc0
Juanesduque1 <juanesduquemorales@gmail.com>
2023-02-09 12:06:57 -0500

    [Fix]: Pycodestyle corrected

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -56,6 +56,6 @@ class Rectangle:
         for i in range(0, self.__height):
             for j in range(0, self.__width):
                 string += "#"
-            if i != self.__height -1:
+            if i != self.__height - 1:
                 string += "\n"
         return string
