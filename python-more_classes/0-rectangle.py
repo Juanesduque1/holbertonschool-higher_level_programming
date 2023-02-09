554b0e84570e32dc9eeeda12438309f217284d72
Juanesduque1 <juanesduquemorales@gmail.com>
2023-02-09 10:27:39 -0500

    [Feature]: First task and readme added

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defining a class that contains a rectangle"""
+
+
+class Rectangle:
+    """Class containing information about the rectangle"""
+    pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,1 @@
+Readme for more classes in python
