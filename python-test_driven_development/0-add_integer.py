2c1ea34b6c9ba93c5341b59d38c07edeb39009e5
Juanesduque1 <juanesduquemorales@gmail.com>
2023-02-09 17:04:42 -0500

    [Fix]: First regular test corrected

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,9 +7,9 @@ def add_integer(a, b=98):
         b = int
     """
     if not isinstance(a, (int, float)):
-        raise TypeError("a must be integer")
+        raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
-        raise TypeError("b must be integer")
+        raise TypeError("b must be an integer")
 
     a, b = int(a),  int(b)
     return a + b
