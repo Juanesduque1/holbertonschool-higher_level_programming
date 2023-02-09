e73d31daeb44ddf2f37f85695f7c7e3fbba472e7
Juanesduque1 <juanesduquemorales@gmail.com>
2023-02-09 14:51:51 -0500

    [Fix]: Public class attribute "print_symbol" corrected

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -60,7 +60,7 @@ class Rectangle:
             return string
         for i in range(0, self.__height):
             for j in range(0, self.__width):
-                string += Rectangle.print_symbol
+                string += str(self.print_symbol)
             if i != self.__height - 1:
                 string += "\n"
         return string
