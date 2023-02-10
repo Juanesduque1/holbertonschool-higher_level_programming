bd7c815b19674376af2d84067d9a29e3a8bc0630
Juanesduque1 <juanesduquemorales@gmail.com>
2023-02-10 17:10:22 -0500

    [Fix]: Test cases corrected

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -14,23 +14,23 @@ class TestMaxInteger(unittest.TestCase):
         """Test case for when max integer is at the beginning"""
         self.assertEqual(max_integer([4, 3, 2, 1]), 4)
 
-    def test_max_at_the_beginning(self):
+    def test_max_in_the_middle(self):
         """Test case for when max integer is in the middle"""
         self.assertEqual(max_integer([3, 4, 2, 1]), 4)
 
-    def test_max_at_the_beginning(self):
+    def test_one_negative_number(self):
         """Test case for when one negative number is on the list"""
         self.assertEqual(max_integer([-1, 2, 3, 4]), 4)
 
-    def test_max_at_the_beginning(self):
+    def test_negative_list(self):
         """Test case for a list with only negative numbers"""
         self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
 
-    def test_max_at_the_beginning(self):
+    def test_one_element_list(self):
         """Test case for a list with only one element"""
         self.assertEqual(max_integer([4]), 4)
 
-    def test_max_at_the_beginning(self):
+    def test_empty_list(self):
         """Test case for an empty list"""
         self.assertEqual(max_integer([]), None)
 
