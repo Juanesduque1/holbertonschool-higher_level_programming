351ea814af8d889e66d0030978980d773bc16436
Juanesduque1 <juanesduquemorales@gmail.com>
2023-02-10 10:50:26 -0500

    [Fix]: Only one argument conditional added

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -17,4 +17,8 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    print(f"My name is {first_name} {last_name}")
+    """Only one argument validation"""
+    if last_name == "":
+        print(f"My name is {first_name}")
+    else:
+        print(f"My name is {first_name} {last_name}")
