fa54a0f355b7fb22e5a4100d10f2bc20737fe2f4
Juanesduque1 <juanesduquemorales@gmail.com>
2023-02-13 15:49:59 -0500

    [Fix]: Documentation of first module

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""Function that returns a list of attributes and methods of an object"""
+
+
 def lookup(obj):
+    """Return of the list with all the information about the object"""
     return dir(obj)
