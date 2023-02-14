4b7a6a3a72838da0d904831bbc990ad231db7197
Juanesduque1 <juanesduquemorales@gmail.com>
2023-02-14 18:01:11 -0500

    [Feature]: README.md creation and Task 0 function completed

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module to create a function"""
+
+
+def read_file(filename=""):
+    """Function to read and write a text file"""
+    with open(filename, 'r') as file:
+        print(file.read())
---
+++ python-input_output/README.md
@@ -0,0 +1,1 @@
+Readme for input/output Project in Python
