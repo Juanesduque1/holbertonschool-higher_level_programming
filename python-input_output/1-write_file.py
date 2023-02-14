2c5365835f44608a8639d771e6e319c5613744b0
Juanesduque1 <juanesduquemorales@gmail.com>
2023-02-14 18:28:36 -0500

    [Fix]: Function corrected

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -2,12 +2,9 @@
 """Module to create a function"""
 
 
-def write_file(filename="", text=""):    
+def write_file(filename="", text=""):
     """Function to read text and write to text file"""
     with open(filename, 'w', encoding="utf-8") as file:
         file.write(text)
-    
-    return len(text)
 
-nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
-print(nb_characters)
+    return len(text)
