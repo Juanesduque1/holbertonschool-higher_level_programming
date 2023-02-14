a3ad74e8fc7d867e4e392415a1326a6eb4d3e8f1
Juanesduque1 <juanesduquemorales@gmail.com>
2023-02-14 16:54:41 -0500

    [Fix]: Subclass error corrected

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,21 +1,8 @@
 #!/usr/bin/python3
 """Defining an empty class"""
 
-
-class BaseGeometry:
-    """Class containing information about basic geometry"""
-
-    def area(self):
-        """Public instance to raise an error"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Function to validate if an argument is an integer greater than 0"""
-        if not type(value) is int:
-            raise TypeError(f"{name} must be an integer")
-
-        elif value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""Import for BaseGeometry class"""
 
 
 class Rectangle(BaseGeometry):
