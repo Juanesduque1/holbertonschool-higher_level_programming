b550356ae7b79cdb8215970f3d7ac155aa283a86
Juanesduque1 <juanesduquemorales@gmail.com>
2023-02-16 12:53:24 -0500

    [Fix]: Pycodestyle corrected

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """Script to add all arguments to a Python list, then save them to a file"""
+
+
 import sys
 import os.path
-
 """Import of previous functions"""
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 """Creation of variables"""
 arguments = sys.argv
-filename = 'add_item.json'
+filename = "add_item.json"
 list = []
 
 """Validating if file exists, if it does, save its content in a list"""
