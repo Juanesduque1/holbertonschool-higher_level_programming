5466543a2ec1d6487ac8e0c3296e2a365074b617
Juanesduque1 <juanesduquemorales@gmail.com>
2023-02-16 15:12:40 -0500

    [Feature]: Task 10-11 initial update

---
+++ python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Class defines a student by certain parameters"""
+
+
+class Student:
+    """Class definition"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialization of constructor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Public method that returns the __dict__ attributes of 'obj'"""
+        new_dict = {}
+
+        if attrs is None:
+            return self.__dict__
+
+        for i in self.__dict__.keys():
+            if i in attrs:
+                new_dict[i] = self.__dict__[i]
+
+        return new_dict
