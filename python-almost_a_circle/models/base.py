14cda5dc27a80beba5cd6592979bfbe0757cb498
Juanesduque1 <juanesduquemorales@gmail.com>
2023-02-24 14:07:13 -0500

    [Fix]: Base class fixed

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -21,7 +21,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Static method to return a JSON string"""
-        if list_dictionaries is None:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
 
