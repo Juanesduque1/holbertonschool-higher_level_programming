271a4aed33a7249c919b36108e3e5b1ffa066ca5
Juanesduque1 <juanesduquemorales@gmail.com>
2023-02-23 19:13:17 -0500

    [Fix]: Base class fixed

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -23,8 +23,7 @@ class Base:
         """Static method to return a JSON string"""
         if list_dictionaries is None:
             return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
 
     @staticmethod
     def from_json_string(json_string):
@@ -52,7 +51,7 @@ class Base:
         if cls.__name__ == "Rectangle":
             obj = cls(1, 1)
 
-        if cls.__name__ == "Square":
+        elif cls.__name__ == "Square":
             obj = cls(1)
 
         obj.update(**dictionary)
@@ -66,7 +65,7 @@ class Base:
         if not os.path.exists(filename):
             return []
 
-        with open(filename, 'r') as file:
+        with open(filename, "r") as file:
             data = Base.from_json_string(file.read())
         list = [cls.create(**dict) for dict in data]
         return list
