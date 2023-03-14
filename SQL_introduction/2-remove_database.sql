8cf7f003c1688be94fbccb3302ca8ebf3133263d
Juanesduque1 <juanesduquemorales@gmail.com>
2023-03-14 11:44:46 -0500

    [Fix]: Remove and create database and insert value files corrected

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
 -- Script that removes a database called 'hbtn_0c_0'
-DROP DATABASE hbtn_0c_0;
+DROP DATABASE IF EXISTS hbtn_0c_0;
--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
 -- Script that inserts a new row in  the table 'first_table'
-INSERT INTO first_table (id, name)
-VALUES (89, Best School);
+INSERT INTO first_table (`id`, `name`) VALUES (89, "Best School");
